81cd1ec894878e17a1a5bee9eeac7eb23324d30c
IgorSilva1990 <igor.silva.dev1990@gmail.com>
2025-01-25 11:50:42 -0300

    joao feito

---
+++ desafio3 - joao.py
@@ -0,0 +1,38 @@
+numero = int(input("Digite um numero para verificar sua tabuada:"))
+
+multium = numero * 1
+multidois = numero * 2
+multitres = numero * 3
+multiquadro = numero * 4
+multicinco = numero * 5
+multiseis = numero * 6
+multisete = numero * 7
+multioito = numero * 8
+multinove = numero * 9
+multidez = numero * 10
+
+print(" ")
+print(" ")
+print(" - - - TABUADA - - - ")
+print(f"TABUADA DO {numero}")
+print(" ")
+
+print(f"multiplicado por um: {multium}")
+print(f"multiplicado por dois: {multidois}")
+print(f"multiplicado por três: {multitres}")
+print(f"multiplicado por quatro: {multiquadro}")
+print(f"multiplicado por cinco: {multicinco}")
+print(f"multiplicado por seis: {multiseis}")
+print(f"multiplicado por sete: {multisete}")
+print(f"multiplicado por oito: {multioito}")
+print(f"multiplicado por nove: {multinove}")
+print(f"multiplicado por dez: {multidez}")
+
+
+
+
+
+
+
+
+
